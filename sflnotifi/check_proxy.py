68b9fc63e2f831cdcbbbb2382256d6c85cdd5c50
subashgubaju10-coder <subashgubaju10@gmail.com>
2026-04-08 03:14:09 +0545

    Delete sflnotifi directory

--- sflnotifi/check_proxy.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-for path in ['/api/farm', '/api/farm2', '/api/farm3']:
-    try:
-        with urllib.request.urlopen(f'http://localhost:9000{path}', timeout=20) as r:
-            print(path, r.status, r.getheader('X-Cache'), len(r.read()))
-    except Exception as e:
-        print(path, 'ERROR', type(e).__name__, e)
